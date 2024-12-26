46e21a1aafb0528bce7286e2ad37c3a9a548ed20
John Groton <john.groton@mymail.champlain.edu>
2024-12-26 10:42:57 -0500

    adding tailwind and trying to create landing page

--- catchup/app/views.py
+++ catchup/app/views.py
@@ -2,3 +2,6 @@ from django.shortcuts import render, HttpResponse
 
 def home(request):
     return render(request, "home.html") 
+
+def landing(request):
+    return render(request, "landing.html") 
---
+++ catchup/tailwind.config.js
@@ -0,0 +1,10 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  purge: [
+      './**/templates/*.html',
+  ],
+  theme: {
+      extend: {},
+  },
+  plugins: [],
+}
