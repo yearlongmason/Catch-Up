77c12a4ea668f7e17fd4855b4aab472dceb2bf2d
John Groton <john.groton@mymail.champlain.edu>
2025-01-13 12:53:33 -0500

    dead

--- catchup/catchup/settings.py
+++ catchup/catchup/settings.py
@@ -76,8 +76,12 @@ WSGI_APPLICATION = 'catchup.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.mysql',
+        'NAME': 'catchupdb',
+        'USER': 'root',
+        'PASSWORD': '2ndACR9!1',
+        'HOST':'localhost',
+        'PORT':'3306',
     }
 }
 
