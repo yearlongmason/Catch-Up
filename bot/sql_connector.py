501796a46b794a2712a147e85367722332289f2d
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-02-04 11:17:57 -0500

    fixing bot insert bug

--- bot/sql_connector.py
+++ bot/sql_connector.py
@@ -12,16 +12,25 @@ db = mysql.connector.connect(
     database=os.getenv('MYSQL_DB')
     )
 
-cursor = db.cursor()
+cursor = db.cursor(buffered=True)
 
 # main is mostly just for testing stuff this file is not meant to be run
 def main():
-    print(os.getenv('MYSQL_DB'))
+    #print(os.getenv('MYSQL_DB'))
+    get_quote_id()
 
 
 def insert_quote(quote: str, server_id: str, author: str):
-    cursor.execute(f'INSERT INTO app_quotes (quote_id, server_id, quote, author, date_quoted) VALUES ("{"1"}, {server_id}", "{quote}", "{author}", "{date.today()}");')
+    quote_id = get_quote_id()
+    cursor.execute(f'INSERT INTO app_quotes (quote_id, server_id, quote, author, date_quoted) VALUES ({quote_id}, "{server_id}", "{quote}", "{author}", "{date.today()}");')
     db.commit()
+
+def get_quote_id():
+    cursor.execute(f'SELECT quote_id FROM app_quotes WHERE quote_id = (SELECT MAX(quote_id) FROM app_quotes)')
+    db.commit()
+    quote_id = cursor.fetchone()
+    #print(quote_id[0])
+    return quote_id[0] + 1
 
 # main is mostly just for testing stuff this file is not meant to be run
 if  __name__ == "__main__":
