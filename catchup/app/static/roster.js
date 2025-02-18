5861651a44c655a1920b4e9aeb4a13e765fcde70
yearlongmason <mdudelee@gmail.com>
2025-02-17 19:27:57 -0500

    Worked more on interactivity using js

--- catchup/app/static/roster.js
+++ catchup/app/static/roster.js
@@ -1,9 +1,18 @@
+// If Django is caching this file, clear your browser cache!
+// In Chrome press ctrl + shift + del and delete cached images and files!
+
 dateAscending = true
-const data = JSON.parse(
-    document.currentScript.nextElementSibling.textContent
-);
 
 function testJSFunc() {
     alert("The JS file is connected and working!")
     dateAscending = false
+}
+
+// This function should essentially scrape all the data from the current roster.html
+// This is a workaround to having to pass a json object through contexts
+function getData() {
+    let quotesList = document.getElementById("allQuotes")
+    let allQuotes = document.getElementsByClassName("quote")
+    console.log(quotesList)
+    console.log(allQuotes)
 }
