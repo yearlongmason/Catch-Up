dd9a3e2c843c1a7dac9b62ba0d0b4645c0a5c93e
yearlongmason <mdudelee@gmail.com>
2025-02-19 02:07:53 -0500

    Finished getData function

--- catchup/app/static/roster.js
+++ catchup/app/static/roster.js
@@ -10,14 +10,21 @@ function testJSFunc() {
 
 // This function should essentially scrape all the data from the current roster.html
 // This is a workaround to having to pass a json object through contexts
+// Returns a list of objects describing each quote
 function getData() {
     let allQuoteTags = document.getElementsByClassName("catchUpQuote")
+    let allQuotes = []
 
     // Loop through each quote div it found
     for(let i = 0; i < allQuoteTags.length; i++){
-        console.log(i, allQuoteTags[i])
-        console.log(allQuoteTags[i].getElementsByClassName("catchUpQuoteText")[0].innerHTML.slice(0, -2))
-        console.log(allQuoteTags[i].getElementsByClassName("catchUpQuoteAuthor")[0].innerHTML)
-        console.log(allQuoteTags[i].getElementsByClassName("catchUpQuoteDate")[0].innerHTML)
+        // Individually get the quote (text), author, and date
+        // Put all values into an object
+        // Append new object to allQuotes array
+        currText = allQuoteTags[i].getElementsByClassName("catchUpQuoteText")[0].innerHTML.slice(0, -2)
+        currAuthor = allQuoteTags[i].getElementsByClassName("catchUpQuoteAuthor")[0].innerHTML
+        currDate = allQuoteTags[i].getElementsByClassName("catchUpQuoteDate")[0].innerHTML.slice(1, -1)
+        allQuotes.push({"quote":currText, "author":currAuthor, "date":currDate})
     }
+
+    return allQuotes
 }
