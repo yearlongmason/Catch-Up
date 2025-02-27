58815244711fa70f7a07975421f84760cd3715a7
yearlongmason <mdudelee@gmail.com>
2025-02-27 03:31:30 -0500

    Finished sorting feature

--- catchup/app/static/roster.js
+++ catchup/app/static/roster.js
@@ -4,17 +4,13 @@
 // Define all global variables
 // ALL_DATA is not technically a constant, but should not be changed
 let ALL_DATA
-let currentData
 let dateNewestFirst
 
 window.onload = (event) => {
-    ALL_DATA = getData()
+    ALL_DATA = getData().reverse() // Set allData constant
     dateNewestFirst = true
+    updateRoster() // Call update roster to make sure it's sorted correctly
 };
-
-function testJSFunc() {
-    alert("The JS file is connected and working!")
-}
 
 // This swaps the dateNewestFirst boolean and updates the button text accordingly
 function swapDateOrder() {
@@ -27,6 +23,48 @@ function swapDateOrder() {
         document.getElementById("sortDateButton").innerText = "Sort date: Newest First"
         dateNewestFirst = true
     }
+}
+
+// Takes in list of JSON object same as ALL_DATA
+// Returns data formatted as HTML to replace in roster.html's allQuotes div
+function formatAsHTML(modifiedData){
+    let formattedhtml = ""
+
+    // Loop through each quote and add that roster element to the new html
+    for (let i = 0; i < modifiedData.length; i++) {
+        formattedhtml += '<div class="container mx-auto mt-6 w-4/5">'
+        formattedhtml += '<div class="bg-white p-6 rounded shadow-md mb-4 h-16 catchUpQuote">'
+        formattedhtml += `<p class="text-gray-800 float-left catchUpQuoteText">${modifiedData[i].quote} -</p>`
+        formattedhtml += `<p class="text-gray-800 float-left pl-1 catchUpQuoteAuthor">${modifiedData[i].author}</p>`
+        formattedhtml += `<p class="text-gray-800 float-right catchUpQuoteDate">(${modifiedData[i].date})</p>`
+        formattedhtml += "</div></div>"
+    }
+
+    // Return the data formatted as HTML
+    return formattedhtml
+}
+
+// Takes in list of JSON object same as ALL_DATA
+// Returns currentData sorted depending on the dateNewestFirst variable
+function sortByDate(currentData) {
+    // Comparison function for sort
+    function compareFn(a, b) { return a - b; }
+
+    // If the date is newest first, just return the sorted list
+    // Otherwise return the reversed sorted list
+    if (dateNewestFirst){
+        return currentData.sort(compareFn)
+    }
+    return currentData.sort(compareFn).reverse()
+}
+
+// When the user has set their preferences they can apply their changes by pressing the update roster button
+// which calls this function to apply all changes
+function updateRoster(){
+    // Make a deepcopy of ALL_DATA
+    currentData = structuredClone(ALL_DATA)
+    currentData = sortByDate(currentData) // Sort by date
+    document.getElementById("allQuotes").innerHTML = formatAsHTML(currentData) // Set inner HTML as new formatted HTML
 }
 
 // This function should essentially scrape all the data from the current roster.html
