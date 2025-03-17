3736c7a95aabaa9c3c2bdc831fc924ca0115fd58
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-03-17 09:50:35 -0400

    user can't access api but it crashes

--- catchup/api/views.py
+++ catchup/api/views.py
@@ -3,13 +3,22 @@ from rest_framework import generics
 from .serializers import QuoteSerializer, IdSerializer, RandomQuoteSerializer
 import requests
 from django.apps import apps
+from django.contrib.admin.views.decorators import staff_member_required
+from django.views.generic import TemplateView
+from django.utils.decorators import method_decorator
+
+def is_admin(user):
+    return user.is_superuser
 
 
+@method_decorator(staff_member_required, name='dispatch')
 class QuoteCreate(generics.ListCreateAPIView):
     Quotes = apps.get_model('app', 'Quotes')
     queryset = Quotes.objects.all()
     serializer_class = QuoteSerializer
 
+
+@method_decorator(staff_member_required, name='dispatch')
 class GetID(generics.ListAPIView):
     Quotes = apps.get_model('app', 'Quotes')
     serializer_class = IdSerializer
@@ -18,6 +27,8 @@ class GetID(generics.ListAPIView):
         server_id = self.request.data['server_id']
         return self.Quotes.objects.filter(server_id = server_id)
 
+
+@method_decorator(staff_member_required, name='dispatch')
 class GetRandomQuote(generics.ListAPIView):
     Quotes = apps.get_model('app', 'Quotes')
     serializer_class = RandomQuoteSerializer
