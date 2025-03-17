c76e5784b1e4a668cc179a2ead213d6e73546bdf
yearlongmason <mdudelee@gmail.com>
2025-03-17 16:56:58 -0400

    Rearranged roster.js functions for a more logical order

--- catchup/app/static/roster.js
+++ catchup/app/static/roster.js
@@ -7,20 +7,36 @@ let ALL_DATA
 let dateNewestFirst
 
 window.onload = (event) => {
-    dateNewestFirst = true
-    ALL_DATA = getData().reverse() // Set allData constant
-    // Format all authors as titled
-    populateDropdown()
+    dateNewestFirst = false
+    ALL_DATA = getData() // Set allData constant
+    populateAuthorsDropdown()
     updateRoster() // Call update roster to make sure it's sorted correctly
 };
 
-// Returns an array of unique names in the quote list
-function getUniqueNames() {
-    return Array.from(new Set(ALL_DATA.map((quote) => quote.author)));
+// beep boop beep boop
+// This function should essentially scrape all the data from the current roster.html
+// This is a workaround to having to pass a json object through contexts
+// Returns a list of objects describing each quote
+function getData() {
+    let allQuoteTags = document.getElementsByClassName("catchUpQuote")
+    let allQuotes = []
+
+    // Loop through each quote div it found
+    for(let i = 0; i < allQuoteTags.length; i++){
+        // Individually get the quote (text), author, and date
+        // Put all values into an object
+        // Append new object to allQuotes array
+        currText = allQuoteTags[i].getElementsByClassName("catchUpQuoteText")[0].innerHTML.slice(0, -2)
+        currAuthor = allQuoteTags[i].getElementsByClassName("catchUpQuoteAuthor")[0].innerHTML
+        currDate = allQuoteTags[i].getElementsByClassName("catchUpQuoteDate")[0].innerHTML.slice(1, -1)
+        allQuotes.push({"quote":currText, "author":currAuthor, "date":currDate, "timestamp":Date.parse(currDate)})
+    }
+
+    return allQuotes
 }
 
 // Populates the dropdown menu with names of people being quoted
-function populateDropdown() {
+function populateAuthorsDropdown() {
     // Add the check all button
     dropdownHTML = `<button id="checkAllButton" onclick="checkAllDropdown()" class="w-full text-left px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer font-bold">Check/Uncheck All</button>`
     // Add a new checkbox for each unique name in the quote list
@@ -28,6 +44,11 @@ function populateDropdown() {
         dropdownHTML += `<label class="flex items-center px-4 py-2 text-gray-700 hover:bg-gray-100 cursor-pointer authorCheckbox"><input type="checkbox" class="mr-2 option-checkbox" checked>${name}</label>`
     });
     document.getElementById("dropdownMenu").innerHTML = dropdownHTML;
+}
+
+// Returns an array of unique names in the quote list
+function getUniqueNames() {
+    return Array.from(new Set(ALL_DATA.map((quote) => quote.author)));
 }
 
 // Make the dropdown menu visible when clicked
@@ -52,6 +73,38 @@ function checkAllDropdown() {
     checkboxes.forEach(checkbox => checkbox.checked = !allChecked);
 }
 
+// Return all authors who are checked in the dropdown menu
+function getCheckedAuthors(){
+    // Get all author checkboxes
+    let authors = Array.prototype.slice.call( document.getElementsByClassName("authorCheckbox") )
+    // Filter by authors who are checked
+    authors = authors.filter((author) => author.children[0].checked)
+
+    // Return the author names
+    return authors.map((author) => author.innerHTML.split(">")[author.innerHTML.split(">").length - 1])
+}
+
+// Filter by authors
+function filterByAuthors(currentData) {
+    // Get all checked authors and return quotes by those authors
+    let checkedAuthors = getCheckedAuthors()
+    return currentData.filter((quote) => checkedAuthors.includes(quote.author))
+}
+
+// Takes in list of JSON object same as ALL_DATA
+// Returns currentData sorted depending on the dateNewestFirst variable
+function sortByDate(currentData) {
+    // Comparison function for sort
+    function compareFn(a, b) { return a.timestamp - b.timestamp; }
+
+    // If the date is newest first, just return the sorted list
+    // Otherwise return the reversed sorted list
+    if (dateNewestFirst){
+        return currentData.sort(compareFn)
+    }
+    return currentData.sort(compareFn).reverse()
+}
+
 // This swaps the dateNewestFirst boolean and updates the button text accordingly
 function swapDateOrder() {
     // If the date is ascending, then swap it to descending
@@ -71,8 +124,24 @@ function swapDateOrder() {
 function updateDateOrderHTML() {
     let currentData = getData() // Gets all the current data
     currentData = sortByDate(currentData) // Sort by date
-    console.log(dateNewestFirst, currentData)
     document.getElementById("allQuotes").innerHTML = formatAsHTML(currentData) // Set inner HTML as new formatted HTML
+}
+
+// Takes in list of JSON object same as ALL_DATA
+// Returns currentData filtered by the string in the search bar
+// (Only returns quotes that have search string as a substring)
+// Searching is not case sensitive
+function filterBySearchBar(currentData) {
+    // Get the contents of the search bar
+    wordToSearch = document.getElementById("searchQuotes").value.toLowerCase()
+
+    // If the user doesn't have anything in the search bar, just skip it
+    if (wordToSearch == "") {
+        return currentData
+    }
+
+    // Return all quotes that have the search string present in it
+    return currentData.filter((item) => item.quote.toLowerCase().includes(wordToSearch))
 }
 
 // Takes in list of JSON object same as ALL_DATA
@@ -95,55 +164,6 @@ function formatAsHTML(modifiedData){
     return formattedhtml
 }
 
-// Takes in list of JSON object same as ALL_DATA
-// Returns currentData sorted depending on the dateNewestFirst variable
-function sortByDate(currentData) {
-    // Comparison function for sort
-    function compareFn(a, b) { return a.timestamp - b.timestamp; }
-
-    // If the date is newest first, just return the sorted list
-    // Otherwise return the reversed sorted list
-    if (dateNewestFirst){
-        return currentData.sort(compareFn)
-    }
-    return currentData.sort(compareFn).reverse()
-}
-
-// Takes in list of JSON object same as ALL_DATA
-// Returns currentData filtered by the string in the search bar
-// (Only returns quotes that have search string as a substring)
-// Searching is not case sensitive
-function filterBySearchBar(currentData) {
-    // Get the contents of the search bar
-    wordToSearch = document.getElementById("searchQuotes").value.toLowerCase()
-
-    // If the user doesn't have anything in the search bar, just skip it
-    if (wordToSearch == "") {
-        return currentData
-    }
-
-    // Return all quotes that have the search string present in it
-    return currentData.filter((item) => item.quote.toLowerCase().includes(wordToSearch))
-}
-
-// Return all authors who are checked in the dropdown menu
-function getCheckedAuthors(){
-    // Get all author checkboxes
-    let authors = Array.prototype.slice.call( document.getElementsByClassName("authorCheckbox") )
-    // Filter by authors who are checked
-    authors = authors.filter((author) => author.children[0].checked)
-
-    // Return the author names
-    return authors.map((author) => author.innerHTML.split(">")[author.innerHTML.split(">").length - 1])
-}
-
-// Filter by authors
-function filterByAuthors(currentData) {
-    // Get all checked authors and return quotes by those authors
-    let checkedAuthors = getCheckedAuthors()
-    return currentData.filter((quote) => checkedAuthors.includes(quote.author))
-}
-
 // When the user has set their preferences they can apply their changes by pressing the update roster button
 // which calls this function to apply all changes
 function updateRoster(){
@@ -153,28 +173,6 @@ function updateRoster(){
     currentData = filterBySearchBar(currentData) // Filter by search word
     currentData = filterByAuthors(currentData) // Filter by authors
     document.getElementById("allQuotes").innerHTML = formatAsHTML(currentData) // Set inner HTML as new formatted HTML
-}
-
-// beep boop beep boop
-// This function should essentially scrape all the data from the current roster.html
-// This is a workaround to having to pass a json object through contexts
-// Returns a list of objects describing each quote
-function getData() {
-    let allQuoteTags = document.getElementsByClassName("catchUpQuote")
-    let allQuotes = []
-
-    // Loop through each quote div it found
-    for(let i = 0; i < allQuoteTags.length; i++){
-        // Individually get the quote (text), author, and date
-        // Put all values into an object
-        // Append new object to allQuotes array
-        currText = allQuoteTags[i].getElementsByClassName("catchUpQuoteText")[0].innerHTML.slice(0, -2)
-        currAuthor = allQuoteTags[i].getElementsByClassName("catchUpQuoteAuthor")[0].innerHTML
-        currDate = allQuoteTags[i].getElementsByClassName("catchUpQuoteDate")[0].innerHTML.slice(1, -1)
-        allQuotes.push({"quote":currText, "author":currAuthor, "date":currDate, "timestamp":Date.parse(currDate)})
-    }
-
-    return allQuotes
 }
 
 // Adds keypress listener
