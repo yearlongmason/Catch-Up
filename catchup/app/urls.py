44f8346df82e441376163ccde0e43ed650d4adf7
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-03-20 17:36:51 -0400

    adding minigame selction page

--- catchup/app/urls.py
+++ catchup/app/urls.py
@@ -10,7 +10,8 @@ urlpatterns  = [
     path("discord_login/",views.discord_login,name="discord_login"),
     path('discord_login/redirect/', views.discord_login_redirect, name="discord_login_redirect"),
     path('servers/', views.servers, name="servers"),
-    path('about/', views.about, name="about")
+    path('about/', views.about, name="about"),
+    path('servers/roster/minigames', views.mingames, name="mingames")
 ]
 
 urlpatterns += staticfiles_urlpatterns()
