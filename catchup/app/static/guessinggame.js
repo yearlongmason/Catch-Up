36712ea898c71c095eacf122da063d856949a78d
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-03-27 10:31:52 -0400

    got quote into html page for guessing game, still got issues though

--- catchup/app/static/guessinggame.js
+++ catchup/app/static/guessinggame.js
@@ -1,28 +1,20 @@
 const correctAuthor = "Franklin D. Roosevelt";
+var quote;
+var rand_index;
 
-
-    function randomQuoteRequest() {     
+    function randomQuoteRequest() {   
+      var quote_element = document.getElementById("quote");
+      quote_element.textContent = "Loading Quote..."  
       const other_params = {
         method: 'GET', 
         headers: {
           'Authorization': `Api-Key ${api_key}`,
           'Content-Type': 'application/json'
-        },
-        data : {
-          'server_id' : server_id
-        },
+        }
       };
+      const URL = api_url + `${server_id}/`;
       
-      fetch(api_url, {
-        method: 'GET', 
-        headers: {
-          'Authorization': `Api-Key ${api_key}`,
-          'Content-Type': 'application/json'
-        },
-        data :JSON.stringify({
-          'server_id' : server_id
-        }),
-       })
+      fetch(URL, other_params)
         .then(response => {
           if (response.ok) {
             return response.json(); 
@@ -30,9 +22,20 @@ const correctAuthor = "Franklin D. Roosevelt";
             throw new Error('API request failed');
           }
         })
+        .then(data => {
+          rand_index = Math.floor(Math.random() * data.length);
+          quote = data[rand_index]
+          //console.log(data[rand_index])
+          //console.log(quote)
+        })
         .catch(error => {
           console.error(error);
         });
+    }
+
+    function populateQuoteHtml() {
+      var quote_element = document.getElementById("quote");
+      quote_element.textContent = quote["quote"];
     }
 
     function checkAnswer() {
@@ -52,5 +55,11 @@ const correctAuthor = "Franklin D. Roosevelt";
         var guessButton = document.getElementById('guessButton');
         guessButton.onclick = checkAnswer;
 
-        console.log(randomQuoteRequest());
+        randomQuoteRequest();
+        
+        // gotta be a better way to do this
+        console.log("Waiting for api call to finish")
+        setTimeout(function(){
+          populateQuoteHtml();
+        }, 2000);
     };
