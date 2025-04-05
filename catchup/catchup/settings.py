609fed36119869a7165c57f6eef8172c056523c1
JohnnyG1234 <89805746+JohnnyG1234@users.noreply.github.com>
2025-04-05 13:04:56 -0400

    changing debug mode

--- catchup/catchup/settings.py
+++ catchup/catchup/settings.py
@@ -27,7 +27,7 @@ SESSION_COOKIE_AGE = 259200
 SECRET_KEY = os.getenv('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
