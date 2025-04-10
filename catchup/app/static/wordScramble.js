025e1e30e9b18386f04a34449da1250c7aa008cd
John Groton <john.groton@mymail.champlain.edu>
2025-04-09 21:46:39 -0400

    adding enter button listener for word scramble ty mason

--- catchup/app/static/wordScramble.js
+++ catchup/app/static/wordScramble.js
@@ -15,3 +15,14 @@ function getNewQuote() {
     window.location.reload();
 }
 
+// Adds keypress listener
+document.addEventListener("keypress", function(event) {
+    // If the user presses the enter key click the update roster button
+    if (event.key === "Enter") {
+        // Prevent the defualt action
+        // Not sure if there actually is one right now, but better safe than sorry
+        event.preventDefault();
+        // Trigger the button element with a click
+        document.getElementById("nextSentence").click();
+    }
+});
